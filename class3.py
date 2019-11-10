2ec63f6258cedc351c35f0f147f42ea97edaec38
prachi <prachisapkota21@gmail.com>
2019-11-10 10:08:36 -0800

    prachi

---
+++ class3.py
@@ -0,0 +1,11 @@
+input("name")
+input("age")
+a=int(input("first number"))
+b=int(input("second number"))
+sum= a+b
+diff=a-b
+product=a*b
+div=(a/b)
+print("sum=%s\n diff=%s\n product=%s\n div=%.2f" %(sum,diff,product,div))
+
+
